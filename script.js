5e4f376857cdd83baa3ac20bfa3185206ae7c158
virons-hp <taknikwala@gmail.com>
2024-10-25 16:40:26 +0530

    from added

--- script.js
+++ script.js
@@ -19,3 +19,49 @@ ScrollReveal().reveal(".videoBgContainerHeadingText p", {
   origin: "right",
 });
 
+ScrollReveal().reveal(".iotFeatureSection h1", {
+    ...scrollRevealoption,
+    origin: "bottom",
+  });
+
+
+ScrollReveal().reveal(".iotFeatureDescription", {
+    ...scrollRevealoption,
+    origin: "left",
+  });
+  
+  ScrollReveal().reveal(".iotFeatureSectionImage", {
+    ...scrollRevealoption,
+    origin: "right",
+  });
+
+
+
+// login pop form
+  // Get modal, button, and close elements
+  const modal = document.getElementById("loginModal");
+ 
+  const loginBtn = document.getElementById("loginBtn");
+  const closeBtn = document.getElementsByClassName("close")[0];
+
+// for learn more button 
+function learnMoreBtn() {
+  modal.style.display = "block";
+}
+
+  // Open modal on button click
+  loginBtn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // Close modal when clicking on the close button
+  closeBtn.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // Close modal when clicking outside of modal content
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
